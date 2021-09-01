f9fb3591c9690c1bb70e0bd1e9fff081b1fab355
jordyf15 <jordyferdian88@gmail.com>
2021-09-01 12:42:26 +0700

    created method create_code for ComputerMaker class and break_code method for HumanBreaker class

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require_relative "string"
+
 class Maker
   attr_reader :secret_code
   def initialize
@@ -14,10 +16,21 @@ class HumanMaker < Maker
 end
 
 class HumanBreaker < Breaker
+  def break_code(turn)
+    puts "Turn ##{turn+1}: Type in four numbers (1-6) to guess the secret code."
+    code = gets.chomp.split('')
+    until code.all? {|digit| digit.to_i >=1 && digit.to_i<=6} && code.size==4
+      puts "Your guess should be 4 digits between (1-6).".red
+      code = gets.chomp.split('')
+    end
+    code
+  end
 end
 
 class ComputerMaker < Maker
-
+  def create_code
+    4.times {@secret_code.push(rand(6)+1)}
+  end
 end
 
 class ComputerBreaker < Breaker
@@ -29,8 +42,10 @@ class Game
 end
 
 def main
-  computer_maker = ComputerMaker.new
-  computer_maker.create_code
+  # computer_maker = ComputerMaker.new
+  # computer_maker.create_code
+  human_breaker = HumanBreaker.new
+  human_breaker.break_code(1)
 end
 
 main()
