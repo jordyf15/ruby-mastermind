c03abc6bd28c832419b65c0290e7a54163a13dcd
jordyf15 <jordyferdian88@gmail.com>
2021-09-02 13:15:18 +0700

    refactor code and linting with rubocop

--- main.rb
+++ main.rb
@@ -1,136 +1,130 @@
-require_relative "string"
+# frozen_string_literal: true
+
+require_relative 'string'
 
 module Printable
   def print_one
-    print "  1  ".bg_green.black
-    print " "
+    print '  1  '.bg_green.black
+    print ' '
   end
 
   def print_two
-    print "  2  ".bg_brown.black
-    print " "
+    print '  2  '.bg_brown.black
+    print ' '
   end
 
   def print_three
-    print "  3  ".bg_blue.black
-    print " "
+    print '  3  '.bg_blue.black
+    print ' '
   end
 
   def print_four
-    print "  4  ".bg_magenta.black
-    print " "
+    print '  4  '.bg_magenta.black
+    print ' '
   end
 
   def print_five
-    print "  5  ".bg_cyan.black
-    print " "
+    print '  5  '.bg_cyan.black
+    print ' '
   end
 
   def print_six
-    print "  6  ".bg_gray.black
-    print " "
+    print '  6  '.bg_gray.black
+    print ' '
   end
 
   def gray_peg
-    print " ".bg_gray
-    print " "
+    print ' '.bg_gray
+    print ' '
   end
 
   def red_peg
-    print " ".bg_red
-    print " "
+    print ' '.bg_red
+    print ' '
   end
-
 end
 
 class Maker
   attr_reader :secret_code
+
   def initialize
-    @secret_code=[]
+    @secret_code = []
   end
-end
-
-class Breaker
-
 end
 
 class HumanMaker < Maker
   def create_code
-    puts "Please typein four numbers (1-6) for your secret code."
+    puts 'Please typein four numbers (1-6) for your secret code.'
     code = gets.chomp.split('')
-    until code.all? {|digit| digit.to_i >=1 && digit.to_i<=6} && code.size==4
-      puts "The secret code must be four number long and use numbers between 1-6.".red
+    until code.all? { |digit| digit.to_i >= 1 && digit.to_i <= 6 } && code.size == 4
+      puts 'The secret code must be four number long and use numbers between 1-6.'.red
       code = gets.chomp.split('')
     end
-    @secret_code = code.map{|digit| digit.to_i}
+    @secret_code = code.map(&:to_i)
   end
 end
 
-class HumanBreaker < Breaker
+class HumanBreaker
   def break_code(turn)
     puts "Turn ##{turn}: Type in four numbers (1-6) to guess the secret code."
     code = gets.chomp.split('')
-    until code.all? {|digit| digit.to_i >=1 && digit.to_i<=6} && code.size==4
-      puts "Your guess should be 4 digits between (1-6).".red
+    until code.all? { |digit| digit.to_i >= 1 && digit.to_i <= 6 } && code.size == 4
+      puts 'Your guess should be 4 digits between (1-6).'.red
       code = gets.chomp.split('')
     end
-    code.map {|digit| digit.to_i}
+    code.map(&:to_i)
   end
 end
 
 class ComputerMaker < Maker
   def create_code
-    4.times {@secret_code.push(rand(6)+1)}
+    4.times { @secret_code.push(rand(1..6)) }
   end
 end
 
-class ComputerBreaker < Breaker
+class ComputerBreaker
   def initialize
-    @possible_codes = [*1111..6666].reject {|code| code.to_s.include?('0') }
+    @possible_codes = [*1111..6666].reject { |code| code.to_s.include?('0') }
   end
 
   def remove_possible_codes(clues, guess_code)
-    new_possible_codes = @possible_codes.select {|code| check_possible_code?(clues, guess_code,code)}
+    new_possible_codes = @possible_codes.select { |code| check_possible_code?(clues, guess_code, code) }
     @possible_codes = new_possible_codes
   end
 
   def check_possible_code?(clues, guess_code, possible_code)
     gray_peg_count = 0
     red_peg_count = 0
-    duplicate_guess_code = guess_code.map{|digit| digit}
-    duplicate_possible_code = possible_code.to_s.split('').map{|digit| digit.to_i}
+    duplicate_guess_code = guess_code.map { |digit| digit }
+    duplicate_possible_code = possible_code.to_s.split('').map(&:to_i)
 
     duplicate_possible_code.each_with_index do |digit, index|
-      if digit == duplicate_guess_code[index]
-        gray_peg_count +=1
-        duplicate_possible_code[index] = nil
-        duplicate_guess_code[index] = nil
-      end
+      next unless digit == duplicate_guess_code[index]
+
+      gray_peg_count += 1
+      duplicate_possible_code[index] = nil
+      duplicate_guess_code[index] = nil
     end
 
     duplicate_possible_code.each_with_index do |digit, index|
-      if digit != nil
-        matched_guess_index = duplicate_guess_code.index(digit)
-        if matched_guess_index !=nil
-          red_peg_count+=1
-          duplicate_possible_code[index]= nil
-          duplicate_guess_code[matched_guess_index] = nil
-        end
-      end
+      next if digit.nil?
+
+      matched_guess_index = duplicate_guess_code.index(digit)
+      next if matched_guess_index.nil?
+
+      red_peg_count += 1
+      duplicate_possible_code[index] = nil
+      duplicate_guess_code[matched_guess_index] = nil
     end
 
-    if [gray_peg_count, red_peg_count] == clues
-      true
-    else
-      false
-    end
+    clues == [gray_peg_count, red_peg_count]
   end
 
   def break_code(turn)
     if turn == 1
-      [1,1,2,2]
+      [1, 1, 2, 2]
     else
-      @possible_codes[0].to_s.split('').map{|digit| digit.to_i}
+      @possible_codes[0].to_s.split('').map(&:to_i)
     end
   end
 end
@@ -138,36 +132,52 @@ end
 class Game
   include Printable
   def introduction
-    puts "Welcome to mastermind"
-    puts "This is a 1 player-game against the computer."
-    puts "Here you can be either the breaker or the maker of the secret code."
-    puts "There are six different number/color combinations."
-    print_one; print_two; print_three; print_four; print_five; print_six
-    puts 
-    puts "The code maker will choose four number to create a 'secret code'. For example,"
-    print_one; print_one; print_four; print_six
+    puts 'Welcome to mastermind'
+    puts 'This is a 1 player-game against the computer.'
+    puts 'Here you can be either the breaker or the maker of the secret code.'
+    puts 'There are six different number/color combinations.'
+    print_one
+    print_two
+    print_three
+    print_four
+    print_five
+    print_six
     puts
-    puts "There can be more than one same number/color in the secret code."
+    puts "The code maker will choose four number to create a 'secret code'. For example,"
+    print_one
+    print_one
+    print_four
+    print_six
+    puts
+    puts 'There can be more than one same number/color in the secret code.'
     puts "To win, the code breaker needs to guess the 'secret code' in 12 or less turns."
-    puts "Clues/Feedback: "
-    puts "After each guess, the breaker will be given up to 4 clue to help them break the code."
-    gray_peg; print "This clue means you have 1 correct number at the correct location.\n"
-    red_peg; print "This clue means you have 1 correct number but at the wrong location.\n"
+    puts 'Clues/Feedback: '
+    puts 'After each guess, the breaker will be given up to 4 clue to help them break the code.'
+    gray_peg
+    print "This clue means you have 1 correct number at the correct location.\n"
+    red_peg
+    print "This clue means you have 1 correct number but at the wrong location.\n"
     puts "For example the secret code '1544' with the guess '4521' will result in the following feedback:"
-    print_four; print_five; print_two; print_one;
+    print_four
+    print_five
+    print_two
+    print_one
     print "\t Clues: "
-    gray_peg; red_peg; red_peg; puts
-    puts "Based on the clue, this means that the guess has 1 number at the correct location and 2 number at the wrong location"
+    gray_peg
+    red_peg
+    red_peg
+    puts
+    puts 'Based on the clue, this means that the guess has 1 number at the correct location and 2 number at the wrong location'
     puts "It's time to play!"
   end
 
   def choose_gameplay
-    puts "Would you like to be the MAKER or the BREAKER of the secret code?"
+    puts 'Would you like to be the MAKER or the BREAKER of the secret code?'
     puts "Enter 1 to be the 'MAKER'"
     puts "Enter 2 to be the 'BREAKER'"
     choice = gets.chomp.to_i
-    until choice == 1 || choice == 2
-      puts "Enter 1 to be the MAKER or 2 to be the BREAKER".red
+    until [1, 2].include?(choice)
+      puts 'Enter 1 to be the MAKER or 2 to be the BREAKER'.red
       choice = gets.chomp.to_i
     end
     if choice == 1
@@ -182,88 +192,92 @@ class Game
   end
 
   private
+
   def breaker_gameplay
     @computer.create_code
     puts "The computer has set it's secret code, and now you have to break it."
     12.times do |turn|
-      breaker_code = @human.break_code(turn+1)
+      breaker_code = @human.break_code(turn + 1)
       maker_code = @computer.secret_code
       if breaker_code == maker_code
-        puts "Congratulations you broke the code"
+        puts 'Congratulations you broke the code'
         print_breaker_code(breaker_code)
         puts
         return
       end
       give_clue(maker_code, breaker_code)
     end
-    puts "Game over, you failed to break the code within 12 turns."
+    puts 'Game over, you failed to break the code within 12 turns.'
   end
 
   def maker_gameplay
     @human.create_code
+    win = true
     12.times do |turn|
-      puts "Computer's turn ##{turn+1}: "
-      breaker_code = @computer.break_code(turn+1)
+      puts "Computer's turn ##{turn + 1}: "
+      breaker_code = @computer.break_code(turn + 1)
       maker_code = @human.secret_code
       if breaker_code == maker_code
         print_breaker_code(breaker_code)
         puts
-        puts "Game over, the computer broke your code."
-        return
+        puts 'Game over, the computer broke your code.'
+        win = false
+        break
       else
         clues = give_clue(maker_code, breaker_code)
         @computer.remove_possible_codes(clues, breaker_code)
       end
     end
-    puts "Congratulations You Win, the computer cannot break your code within 12 turns."
+    puts 'Congratulations You Win, the computer cannot break your code within 12 turns.' if win == true
   end
 
   def give_clue(maker_code, breaker_code)
     gray_peg_count = 0 # grey peg means correct number and correct location
     red_peg_count = 0 # grey peg means correct number but wrong location
-    duplicate_maker_code = maker_code.map {|digit| digit}
-    duplicate_breaker_code = breaker_code.map {|digit| digit}
+    duplicate_maker_code = maker_code.map { |digit| digit }
+    duplicate_breaker_code = breaker_code.map { |digit| digit }
 
     duplicate_breaker_code.each_with_index do |digit, index|
-      if digit == duplicate_maker_code[index]
-        gray_peg_count +=1
-        duplicate_breaker_code[index] = nil
-        duplicate_maker_code[index] = nil
-      end
+      next unless digit == duplicate_maker_code[index]
+
+      gray_peg_count += 1
+      duplicate_breaker_code[index] = nil
+      duplicate_maker_code[index] = nil
     end
 
     duplicate_breaker_code.each_with_index do |digit, index|
-      if digit != nil
-        matched_maker_index = duplicate_maker_code.index(digit)
-        if matched_maker_index !=nil
-          red_peg_count+=1
-          duplicate_breaker_code[index]= nil
-          duplicate_maker_code[matched_maker_index] = nil
-        end
-      end
+      next if digit.nil?
+
+      matched_maker_index = duplicate_maker_code.index(digit)
+      next if matched_maker_index.nil?
+
+      red_peg_count += 1
+      duplicate_breaker_code[index] = nil
+      duplicate_maker_code[matched_maker_index] = nil
     end
 
     print_breaker_code(breaker_code)
     print "\t Clues: "
-    gray_peg_count.times {gray_peg}
-    red_peg_count.times {red_peg}
+    gray_peg_count.times { gray_peg }
+    red_peg_count.times { red_peg }
     puts
     [gray_peg_count, red_peg_count]
   end
 
   def print_breaker_code(breaker_code)
     breaker_code.each do |digit|
-      if digit == 1
+      case digit
+      when 1
         print_one
-      elsif digit == 2
+      when 2
         print_two
-      elsif digit == 3
+      when 3
         print_three
-      elsif digit == 4
+      when 4
         print_four
-      elsif digit == 5
+      when 5
         print_five
-      elsif digit == 6
+      when 6
         print_six
       end
     end
@@ -278,13 +292,13 @@ def main
     game.choose_gameplay
     puts "Do you want to play again? Press 'y' for yes and 'n' for no."
     play_again_input = gets.chomp
-    until play_again_input == 'y' || play_again_input=='n'
+    until %w[y n].include?(play_again_input)
       puts "Please input 'y' to play again or 'n' for no".red
       play_again_input = gets.chomp
     end
-    play_again=false if play_again_input=='n'
+    play_again = false if play_again_input == 'n'
   end
-  puts "Thank you for playing Mastermind, and have a nice day.."
+  puts 'Thank you for playing Mastermind, and have a nice day..'
 end
 
-main()
+main
